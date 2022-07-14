e1ea3b6d67621f943e8598db92631a154c3ef582
agherasie <alexandru.gherasie@epitech.eu>
2022-07-14 11:48:34 +0200

    :adhesive_bandage: Input handling made indirect
    Made the player only move if the same label is predicted multiple times in a short timestamp

--- src/player.py
+++ src/player.py
@@ -11,15 +11,37 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = 400
         self.rect.x = 800
+
+        self.input = [0.0, 0.0, 0.0, 0.0]
+
     def move(self, label):
-        if label == 0:
+        UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
+
+        ##### Indirect movement
+        self.input[label] += 0.1
+        for i in range(len(self.input)):
+            if (i != label):
+                self.input[label] -= 0.2
+            if (self.input[label] < 0):
+                self.input[label] = 0
+        if (self.input[UP] >= 1):
             self.rect.y -= 1
-        if label == 1:
+        if (self.input[DOWN] >= 1):
             self.rect.y += 1
-        if label == 2:
+        if (self.input[LEFT] >= 1):
             self.rect.x -= 1
-        if label == 3:
+        if (self.input[RIGHT] >= 1):
             self.rect.x += 1
+
+        ##### Direct movement
+        # if label == UP:
+        #     self.rect.y -= 1
+        # if label == DOWN:
+        #     self.rect.y += 1
+        # if label == LEFT:
+        #     self.rect.x -= 1
+        # if label == RIGHT:
+        #     self.rect.x += 1
     def reset(self):
         self.rect.y = 400
         self.rect.x = 800
